89d699d8d9aecbff1af59798d104354d897b434c
pritamyadav007 <pritamyadav211191@gmail.com>
2025-04-29 17:56:35 +0530

    Admin added in dashboard

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+     path('', lambda request: redirect('login', permanent=False)),
     path('admin/', admin.site.urls),
     path('hr/', include('hr.urls')),
     path('inventory/',include('inventory.urls')),
