6e6188ba536ee227313dbe1bf3e7f0bca8546bf8
pritamyadav007 <pritamyadav211191@gmail.com>
2025-04-29 11:42:16 +0530

    Intial Setup Done Django Application gives the response properly and intial Configuration Done

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hr/', include('hr.urls')),
+    path('inventory/',include('inventory.urls')),
+    path('sales/', include('sales.urls')),
+    path('production/',include('production.urls')),
 ]
