5cb8fdd6e2ed177e438a4f57a66f3d2a3dfdbc25
Pawan <pawanyadav211191@gmail.com>
2025-05-18 14:37:38 +0530

    Error solved regarding to sales section

--- sales/urls.py
+++ sales/urls.py
@@ -1,14 +1,13 @@
-'''Datta outliar enhnaced code'''
 # sales/urls.py
 from django.urls import path
 from . import views
 
+app_name = 'sales'  # This enables {% url 'sales:bill_list' %} and so on
+
 urlpatterns = [
-    # Customer URLs
     path('customers/', views.customer_list, name='customer_list'),
     path('customers/create/', views.customer_create, name='customer_create'),
-    
-    # Bill URLs
+
     path('bills/', views.bill_list, name='bill_list'),
     path('bills/create/', views.bill_create, name='bill_create'),
     path('bills/<int:pk>/', views.bill_detail, name='bill_detail'),
